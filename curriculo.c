5fd7b28c5820b1f70fc413c3aab448c177ba9f59
jujukeko02-png <jujukeko02@gmail.com>
2026-03-16 22:11:04 -0300

    Update curriculo.c

--- curriculo.c
+++ curriculo.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
 
+struct Curriculo {
+    
 //Variaveis e seus valores//
 char nome [30];
 int idade;
 char datadeNasc [30];
-char email;
+char email [30];
 int numerodeTel;
 char cidade [30];
 char estado [30];
@@ -16,16 +16,39 @@ char referenceTrabalho [30];
 int tempoTrabalhreference;
 char moreSobre [30];
 
+};
+    
+
+int main()
+{
+
+struct Curriculo pessoa;
+
 //Pedindo informações ao usário e recebendo essas informações//
+
 
 printf("Bem Vindo (a) ao Curriculo Por linguagem C,\nSe estiver pronto (a), digite enter\n\n");
 printf("Por favor digite seu nome completo:\n");
-fgets(nome, 30, stdin);
-printf("Nome completo: %s", nome);
+fgets(pessoa.nome, 30, stdin);
+printf("Nome completo: %s", pessoa.nome);
 printf("Digite por favor sua idade:\n");
-scanf("%d",&idade);
-printf("\nIdade Atual: %d\n",idade);
+scanf("%d",&pessoa.idade);
+printf("\nIdade Atual: %d\n",pessoa.idade);    
+printf("Por favor digite sua data de nascimento:\n");
+scanf("%s",&pessoa.datadeNasc);
+printf("Data de Nascimento:%s\n",pessoa.datadeNasc);
+printf("Digite agora o seu email:\n");
+scanf("%s",&pessoa.email);
+printf("Email: %s\n",pessoa.email);
 
+
+
+
+
+
+
+
+ 
 
 return 0;
 }
